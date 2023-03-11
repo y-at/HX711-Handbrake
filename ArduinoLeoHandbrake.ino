d94153ec85c91dae9edd835e974ec0a46eaecf29
Wyatt Bristow <79484754+y-at@users.noreply.github.com>
2023-03-11 13:46:51 -0700

    Added comments, commented out debugging code

--- ArduinoLeoHandbrake.ino
+++ ArduinoLeoHandbrake.ino
@@ -7,25 +7,22 @@ const int LOADCELL_SCK_PIN = 3;
 HX711 scale;
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   Joystick.begin();
   scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
   scale.set_scale();    
   scale.tare();
 }
 
-void setThrottleIf(int in) {
-  
-}
-
 void loop() {
+  # Invert reading and decrease reading values
   int reading = scale.get_units(1) * -0.1;
-  Serial.print("Unmapped reading: ");
-  Serial.println(reading);
+  # For debugging purposes, slows readings when not reading serial monitor
+  #Serial.print("Unmapped reading: ");
+  #Serial.println(reading);
+  # Map reading to 0-248, else windows controller reading loops back to 0
   int readingMapped = map(reading,15000,245000,0,248);
-  if (readingMapped < 20)
-    readingMapped = 0;
   Joystick.setThrottle(readingMapped);
-  Serial.print("Mapped reading: ");
-  Serial.println(readingMapped);
+  #Serial.print("Mapped reading: ");
+  #Serial.println(readingMapped);
 }
